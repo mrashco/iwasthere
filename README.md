45459514ebc34e2cc4a17924bf329acd8edf4879
mrashleyball <me@mrashleyball.com>
2022-07-30 21:13:53 +1000

    Update 1.3.5

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 Teachers do a lot of Professional Development (PD) and sometimes you might get a certificate as evidence, most of the time not so much. This is what I've looked for, a quick and easy certificate generator to use as evidence. No more screenshots of emails!
 
 ## Project
-Built with HTML, Python & PyScript, hosted on GitHub.
+Built with HTML, Python & PyScript, hosted on GitHub. See [Colour Palette](https://coolors.co/081621-3e8ed0-eff6fb).
 
 ## Author
 Hi, I'm Ash an IT teacher, former web designer and I'm currently learning Cyber Security and Python Programming. Follow me @mrashleyball and visist the blog at mrash.co for regular articles and the monthly newsletter.
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fpdf import FPDF
 import io
 from js import document, window, Uint8Array, File
-# from datetime import datetime
+from datetime import datetime
 
 def generate(*args, **kwargs):
     # JS grabs id strings value, stores it
@@ -13,7 +13,9 @@ def generate(*args, **kwargs):
 
     if not fname == '' and not lname == '' and not hours == '' and not event == '':
         pdf = FPDF(orientation='landscape')
+        # cert_image = open("cert.png", "rb", buffering=0)
         pdf.add_page()
+        # pdf.image(cert_image)
         pdf.set_font('helvetica', size=12)
         pdf.cell(txt=f'This certificate is for {fname} {lname}.')
         pdf.cell(txt=f'For attending {event} on {date}.')
@@ -26,18 +28,24 @@ def generate(*args, **kwargs):
         pdf_file = File.new([Uint8Array.new(my_stream.getvalue())], 'hello_world.pdf', {'type': 'application/pdf'})
 
         url = window.URL.createObjectURL(pdf_file)
-
+        time_now = getTimeNow()
+        
+        # Remove spaces and return with dashes for filename
+        fname, lname, event = removeSpaces(fname), removeSpaces(lname), removeSpaces(event)
         link = document.getElementById('generate')
-        link.setAttribute('download', 'certificate.pdf')
+        link.setAttribute('download', f'iwasthere-{fname}-{lname}-{event}-{time_now}.pdf'.lower())
         link.setAttribute('href', url)
         link.setAttribute('text', 'Download')
         # link.setAttribute('class', 'button is-success')
 
-def previewFunc(*args, **kwargs):
-    # object.addEventListener("fname", myScript)
-    x = document.getElementById("fname")
-    x.value = str(x.value).upper()
-    x.addEventListener("keyup", previewFunc)
+def getTimeNow():
+    now = datetime.now()
+    return now.strftime(r'%d-%m-%Y-%H-%M-%S')
 
-    # x = document.getElementById("fname")
-    # x.value = x.value.toUpperCase()
+def removeSpaces(name):
+    new_name = ''
+    for char in name:
+        if char == ' ':
+            char = '-'
+        new_name += char
+    return new_name
